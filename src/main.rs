d726a27d0a657dcba0f54ace836ecae17f93da6a
pravin82 <pravinmishra1796@gmail.com>
2023-10-25 08:49:26 +0530

    Read the input command and implement exit command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
+use std::io;
+use std::io::Write;
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+   while(true){
+      print_prompt();
+      let mut line = String::new();
+      let command_string = std::io::stdin().read_line(&mut line).unwrap();
+      if(line.trim() == ".exit"){
+         exit(0)
+      }
+      else {
+         println!("Unrecognised command")
+      }
+
+   }
     
+}
+
+fn print_prompt(){
+   print!("db > ");
+   io::stdout().flush();
+
 }
 
 
